6b71417fe9433f14fe77603b89e99bc682ad3a5e
FafaelDuarte <155685529+Rafafrias2012@users.noreply.github.com>
2024-08-07 10:25:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,18 +8,25 @@ const settings = require('./settings.json');
 app.use(express.static(path.join(__dirname, 'frontend')));
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+    console.log('A user connected');
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
+    socket.on('join', (data) => {
+        socket.join(data.roomId);
+        socket.nickname = data.nickname;
+        socket.roomId = data.roomId;
+        console.log(`${socket.nickname} joined room ${socket.roomId}`);
+    });
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
+    socket.on('chat message', (data) => {
+        io.to(socket.roomId).emit('chat message', { nickname: socket.nickname, message: data.message });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
 });
 
 const PORT = process.env.PORT || settings.port;
 http.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
